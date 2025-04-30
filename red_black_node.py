ced909cd081e02862625816b9625d7eea3ebbead
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-29 23:41:31 -0700

    child node class of RedBlackNode

--- red_black_node.py
+++ red_black_node.py
@@ -1,4 +1,10 @@
+# Group Members: Michael Jung (ID:10680322), Timothy Sanders (01002147), Megan Ng (ID: 00756276)
 
+# Date: 4/21/25
+
+# Course: Spr25_CS_034 CRN 39575
+
+#--------------------------------------------------------------------------------------------
 
 
 # ---- File: red_black_node.py ----
