98fc4dffb4f8315bf8911e06b3e594f305a0cd26
Michael Jung <96927527+lolMichaelJung1@users.noreply.github.com>
2025-05-03 10:40:36 -0700

    Update main_demo.py
    imported AVLTree and RedBlackTree classes and resolved some errors

--- main_demo.py
+++ main_demo.py
@@ -5,7 +5,8 @@ import sys
 from bst_node import BstNode
 from avl_node import AvlNode
 from red_black_node import RedBlackTreeNode
-
+from AVLTree import AVLTree
+from RedBlackTree import RedBlackTree
 
 if __name__ == "__main__":
     values_to_insert = [10, 20, 30, 15, 25, 5, 1, 8, 18, 28]
@@ -15,17 +16,16 @@ if __name__ == "__main__":
     for value in values_to_insert:
         print(f"\nInserting {value} in AVLTree...")
         avl.insert(value)
-        avl1.print_tree(avl1.root)
+        avl.print_tree(avl.root)
         print()
     print("\nFinal AVLTree Root:", avl.root)
     print()
 
-    
-    print("\n\n------- Demo of Red-Black Tree ------")  
+    print("\n\n------- Demo of Red-Black Tree ------")
     rbt = RedBlackTree()
     for value in values_to_insert:
-        print(f"\nInserting {key} in Red-Black Tree...")
-        rbt.insert(key)
+        print(f"\nInserting {value} in Red-Black Tree...")
+        rbt.insert(value)
         print("Inorder Traversal (key, color):", rbt.inorder_traversal())
         # You would typically add more checks here to verify RBT properties
 
