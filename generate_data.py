3a68c78e685d6892cec515353599f9bc96537af1
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-05-03 12:53:28 -0700

    Not necessary for this lab, may keep for future refer

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,12 @@
+# Created by: Michael Jung (ID:10680322), Timothy Sanders (ID: 01002147), Megan Ng (ID: 00756276)
+
+# Date: 5/1/25
+
+# Course: Spr25_CS_034 CRN 39575
+
+#--------------------------------------------------------------------------------------------
+
+
 import random
 import json
 from typing import List
