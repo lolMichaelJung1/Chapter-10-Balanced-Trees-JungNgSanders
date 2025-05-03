2a214f1f955454a634d22bf3816770efbf628fcc
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-05-03 12:57:58 -0700

    add  demo for avl_inventory_manager & rbt_flight_manager

--- main_demo.py
+++ main_demo.py
@@ -1,5 +1,10 @@
 # Created by: Michael Jung (ID:10680322), Timothy Sanders (ID: 01002147), Megan Ng (ID: 00756276)
 
+# Date: 5/1/25
+
+# Course: Spr25_CS_034 CRN 39575
+
+#--------------------------------------------------------------------------------------------
 import random
 from io import StringIO
 import sys
